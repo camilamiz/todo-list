95e68a862dd2d033fe8060e66c2020669412ad5b
Camila Mizumoto <camila.yuri.m@gmail.com>
2023-05-08 19:54:51 -0300

    add radio button styles

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,12 +1,25 @@
 import { HiOutlineTrash } from 'react-icons/hi'
-
+import { Checkbox } from '@mui/material'
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import RadioButtonUncheckedOutlinedIcon from '@mui/icons-material/RadioButtonUncheckedOutlined';
 import styles from './Task.module.css'
 
 export function Task () {
   return (
     <>
       <div className={styles.card}>
-        <input type="checkbox" />
+        <Checkbox
+        icon={<RadioButtonUncheckedOutlinedIcon />}
+        checkedIcon={<CheckCircleIcon />}
+          sx={{
+            color: 'var(--blue-400)',
+            '&.Mui-checked': {
+              color: 'var(--purple-700)',
+            }
+          }}
+        />
+        <div className={styles.checkbox}>
+        </div>
         <div className={styles.description}>
           Integer urna interdum massa libero auctor massa libero auctor massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
         </div>
